783ffc24215331d17cfbf7a15d58caacef22e56b
Federico Carrasco <federicogcarrasco@gmail.com>
2022-07-23 23:21:43 -0300

    add characterbasicDTO

---
+++ src/main/java/com/alkemy/disney/dto/CharacterBasicDTO.java
@@ -0,0 +1,11 @@
+package com.alkemy.disney.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CharacterBasicDTO {
+    public String name;
+    public String image;
+}
