5bd2861507aed21fd0dcfd8d6b1a185e24ef4c21
Federico Carrasco <federicogcarrasco@gmail.com>
2022-07-26 17:38:47 -0300

    Remove unused import

--- src/main/java/com/alkemy/disney/auth/filter/JwtRequestFilter.java
+++ src/main/java/com/alkemy/disney/auth/filter/JwtRequestFilter.java
@@ -3,7 +3,6 @@ package com.alkemy.disney.auth.filter;
 import com.alkemy.disney.auth.service.JwtUtils;
 import com.alkemy.disney.auth.service.UserDetailsCustomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
