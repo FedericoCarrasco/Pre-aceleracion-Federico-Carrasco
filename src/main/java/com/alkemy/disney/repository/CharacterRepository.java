c4436a5c5c7f96eb8810957756bc9803d01034ba
Federico Carrasco <federicogcarrasco@gmail.com>
2022-07-29 22:22:06 -0300

    Add repository annotations

--- src/main/java/com/alkemy/disney/repository/CharacterRepository.java
+++ src/main/java/com/alkemy/disney/repository/CharacterRepository.java
@@ -4,9 +4,11 @@ import com.alkemy.disney.entity.CharacterEntity;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CharacterRepository extends JpaRepository<CharacterEntity, Long>, JpaSpecificationExecutor<CharacterEntity> {
 
     List<CharacterEntity> findAll(Specification<CharacterEntity> specification);
--- src/main/java/com/alkemy/disney/repository/MovieRepository.java
+++ src/main/java/com/alkemy/disney/repository/MovieRepository.java
@@ -4,9 +4,11 @@ import com.alkemy.disney.entity.MovieEntity;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MovieRepository extends JpaRepository<MovieEntity, Long>, JpaSpecificationExecutor<MovieEntity> {
 
     List<MovieEntity> findAll(Specification<MovieEntity> specification);
